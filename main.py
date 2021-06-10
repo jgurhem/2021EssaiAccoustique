bfa44b9c949126e0ad17382c17bf28ebcf2ca0bc
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2021-06-10 23:07:26 +0200

    new version with normalization and fftshit

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 def readfile(path):
   return np.genfromtxt(path, delimiter=';', skip_header=9, encoding='latin1', dtype = None)
 
-def _plot(data, out_file, bbox_inches = 'tight', metadata = dict()):
+def _plot(x, y, out_file, bbox_inches = 'tight', metadata = dict()):
   fig = plt.figure()
   ax = fig.gca()
-  ax.plot(data)
+  ax.plot(x, y)
   fig.savefig(out_file, bbox_inches = bbox_inches, metadata = {**metadata, 'CreationDate': None})
   plt.close()
   
@@ -24,10 +24,20 @@ data = np.empty(shape = [0, 2])
 for f in files:
   d = readfile(f)
   data = np.append(data, d, axis = 0)
-#  _plot(d, f + '.pdf')
 
-_plot(data[:, 1], DIR + '.pdf')
-fft = np.fft.fft(data[:, 1])
-_plot(fft, DIR + '_fft.pdf')
+Ne = len(data)
+Te = data[1, 0] - data[0, 0]
+print(Te)
 
+_plot(np.arange(Ne) * Te, data[:, 1], DIR + '.pdf')
+
+spectre = np.fft.fftshift(np.fft.fft(data[:, 1])) / Ne
+frequences = np.arange(Ne) * 1.0 / (Te * Ne)
+
+_plot(frequences, np.abs(spectre), DIR + '_fftshit_abs.pdf')
+_plot(frequences, np.real(spectre), DIR + '_fftshit_real.pdf')
+_plot(frequences, np.imag(spectre), DIR + '_fftshit_imag.pdf')
+
+spectre = np.fft.fft(data[:, 1]) / Ne
+_plot(frequences, np.abs(spectre), DIR + '_fft.pdf')
 
