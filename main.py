13d1400807082e23ee6465f9274846a8d2f10617
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2021-07-12 18:26:07 +0200

    Save values and fft as csv files

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ Te = data[1, 0] - data[0, 0]
 print(Te)
 
 _plot(np.arange(Ne) * Te, data[:, 1], DIR + '_val.pdf')
+np.savetxt(DIR + '_val.csv', data[:, 1], delimiter=",")
 
 spectre = np.fft.fftshift(np.fft.fft(data[:, 1])) / Ne
 frequences = np.arange(Ne) * 1.0 / (Te * Ne)
@@ -42,4 +43,5 @@ _plot(frequences, np.imag(spectre), DIR + '_fftshit_imag.pdf')
 
 spectre = np.fft.fft(data[:, 1]) / Ne
 _plot(frequences, np.abs(spectre), DIR + '_fft.pdf')
+np.savetxt(DIR + '_fft.csv', np.abs(spectre), delimiter=",")
 
