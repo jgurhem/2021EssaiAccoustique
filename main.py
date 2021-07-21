50bfcc0ff75405d61ad6b34d21ced4b918fcb6ed
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2021-07-21 22:46:02 +0200

    compute b value

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import re
 import argparse
 import sys
+from scipy import signal
+from scipy import stats
 
 def readfile(path):
   return np.genfromtxt(path, delimiter=';', skip_header=9, encoding='latin1', dtype = None)
@@ -18,7 +20,19 @@ def _plot(x, y, out_file, bbox_inches = 'tight', metadata = dict(), xlim = None)
     plt.xlim(xlim)
   fig.savefig(out_file, bbox_inches = bbox_inches, metadata = {**metadata, 'CreationDate': None})
   plt.close()
-  
+
+def _plot_reg(x, y, out_file, bbox_inches = 'tight', metadata = dict(), xlim = None):
+  fig = plt.figure()
+  ax = fig.gca()
+  ax.plot(x, y)
+  slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+  ax.plot(x, slope * x + intercept)
+  if xlim != None:
+    plt.xlim(xlim)
+  fig.savefig(out_file, bbox_inches = bbox_inches, metadata = {**metadata, 'CreationDate': None})
+  plt.close()
+  return slope, intercept
+
 def sort_func(x):
   r = []
   for s in re.split(r'([^0-9.]+)', x):
@@ -61,3 +75,19 @@ spectre = np.fft.fft(data[:, 1]) / Ne
 _plot(frequences, np.abs(spectre), DIR + '_fft.pdf')
 np.savetxt(DIR + '_fft.csv', np.abs(spectre), delimiter=",")
 
+P=20
+b1 = signal.firwin(numtaps= 2 * P + 1, cutoff = [0.1], window = 'hann', nyq = 0.5)
+a1 = [1.0]
+zi = signal.lfiltic(b1, a1, x=[0], y=[0])
+[y0, zf] = signal.lfilter(b1, a1, data[:, 1], zi=zi)
+_plot(np.arange(Ne) * Te, y0, DIR + '_rif.pdf')
+np.savetxt(DIR + '_rif.csv', y0, delimiter=",")
+
+y0 = np.abs(y0)
+min_ = np.min(y0[y0 > 0])
+log_Ne = np.log(np.arange(1, Ne + 1))
+y0[y0 == 0] = min_
+log_y0 = np.log(np.sort(y0)[::-1])
+a, b = _plot_reg(log_Ne, log_y0, DIR + '_rif_nn.pdf')
+print('slope', a)
+print('intercept', b)
