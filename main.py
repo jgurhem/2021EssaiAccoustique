b99eb969bd2d5a05f07e35f2e69252d9a0bafd8b
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2021-07-21 22:45:18 +0200

    remove fftshift

--- main.py
+++ main.py
@@ -56,13 +56,7 @@ print(Te)
 _plot(np.arange(Ne) * Te, data[:, 1], DIR + '_val.pdf')
 np.savetxt(DIR + '_val.csv', data[:, 1], delimiter=",")
 
-spectre = np.fft.fftshift(np.fft.fft(data[:, 1])) / Ne
 frequences = np.arange(Ne) * 1.0 / (Te * Ne)
-
-_plot(frequences, np.abs(spectre), DIR + '_fftshit_abs.pdf')
-_plot(frequences, np.real(spectre), DIR + '_fftshit_real.pdf')
-_plot(frequences, np.imag(spectre), DIR + '_fftshit_imag.pdf')
-
 spectre = np.fft.fft(data[:, 1]) / Ne
 _plot(frequences, np.abs(spectre), DIR + '_fft.pdf')
 np.savetxt(DIR + '_fft.csv', np.abs(spectre), delimiter=",")
