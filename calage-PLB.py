3e2050438cff6083c0e7dac6d048b0f95b626e96
Anne-Claire <anne.claire.sla@gmail.com>
2021-10-25 23:26:18 +0200

    remove unused parts of the file

--- calage-PLB.py
+++ calage-PLB.py
@@ -112,7 +112,6 @@ print("y0 :", y0)
 
 print(all_values)
 
-number_all_points = int(2000 + all_time/Te)
 valeurs_calees_x = []
 valeurs_calees_y = []
 
@@ -203,98 +202,4 @@ log_y0 = np.log(np.sort(y0)[::-1])
 a, b = _plot_reg(log_Ne, log_y0, DIR + '_rif_nn.pdf')
 print('slope', a)
 print('intercept', b)
-
-
-
-
-
-exit(0)
-
-
-
-Ne = len(y0)
-FileSize = Ne / len(files)
-
-print('FileSize', FileSize)
-print('delta t', Te)
-print('Points', Ne)
-
-Max = max(y0)
-print('amplitude max', Max)
-
-Min = min(y0)
-print('amplitude min', Min)
-
-
-
-print(coups)
-	
-energies = []
-for i in range(len(all_values)):
-  area = trapz(np.square(all_values[i]), relative_dates[i] + np.arange(50000) / 50000 * (relative_dates[i+1] - relative_dates[i]))
-  energies.append(area)
-print(energies)
-
-_plot(relative_dates[:-1], y0, DIR + '_calage.pdf')
-np.savetxt(DIR + '_calage.csv', np.column_stack((relative_dates[:-1], y0)), delimiter=";")
-
-_plot(relative_dates[:-1], energies, DIR + '_calage_energies.pdf')
-np.savetxt(DIR + '_calage_energies.csv', np.column_stack((relative_dates[:-1], energies)), delimiter=";")
-
-_plot(relative_dates[:-1], coups, DIR + '_calage_coups.pdf')
-np.savetxt(DIR + '_calage_coups.csv', np.column_stack((relative_dates[:-1], coups)), delimiter=";")
-
-exit(0)
-
-
-Coups = 0
-for i in y0:
-  if i > ThresholdMax or i < ThresholdMin:
-    Coups = Coups + 1
-
-CrossThresholdStart = 0
-test = True
-while y0[CrossThresholdStart] < ThresholdMax and y0[CrossThresholdStart] > ThresholdMin and CrossThresholdStart < len(y0) - 1:
-  CrossThresholdStart += 1
-
-CrossThresholdEnd = len(y0) - 1
-while y0[CrossThresholdEnd] < ThresholdMax and y0[CrossThresholdEnd] > ThresholdMin and CrossThresholdEnd > 0:
-  CrossThresholdEnd -= 1
-
-CrossMax = 0
-while y0[CrossMax] != Max and CrossMax < len(y0) - 1:
-  CrossMax += 1
-
-print ('CrossThresholdStart', CrossThresholdStart)
-print ('CrossMax', CrossMax)
-print ('CrossThresholdEnd', CrossThresholdEnd)
-
-Aire = 0
-for i in range(len(y0)-1):
-	Aire = (y0[i]**2+y0[i+1]**2)/2*Te
-print('Energie', Aire)
-
-timeStart = Te * (CrossThresholdStart + args.start * FileSize)
-timeMax = Te * (CrossMax + args.start * FileSize)
-timeEnd = Te * (CrossThresholdEnd + args.start * FileSize)
-duration = timeEnd - timeStart
-dureeMontee = timeMax - timeStart
-
-Freqmoy = Coups / duration
-print ('Coups', Coups)
-print ('Duree', duration)
-print ('Temps debut', timeStart)
-print ('Temps Max', timeMax)
-print ('Temps fin', timeEnd)
-print ('Fréquence moyenne', Freqmoy)
-print ('Duree montée', dureeMontee)
-
-_plot(np.arange(args.start * FileSize, args.end * FileSize) * Te, data[:, 1], DIR + '_val.pdf')
-np.savetxt(DIR + '_val.csv', data[:, 1], delimiter=",")
-
-frequences = np.arange(args.start * FileSize, args.end * FileSize) * 1.0 / (Te * Ne)
-spectre = np.fft.fft(data[:, 1]) / Ne
-_plot(frequences, np.abs(spectre), DIR + '_fft.pdf')
-np.savetxt(DIR + '_fft.csv', np.abs(spectre), delimiter=",")
-
 
